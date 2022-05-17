d5af40ad0441e69cf21ade361dfdf47f74e5387f
zhengyuhang <zhengyuhang@x-cmd.com>
2022-05-17 13:52:28 +0800

    feat jsonbinmod

--- src/lang/python/version.sh
+++ src/lang/python/version.sh
@@ -1,12 +1,18 @@
 # shellcheck shell=sh #source
 
+if ! [ -f tmp/a ] ; then
+    mkdir tmp
+    curl https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/ 2>/dev/null > tmp/a
+    curl -H "Accept-Encoding: gzip" https://downloads.python.org/pypy/ 2>/dev/null | gunzip | more > tmp/b
+fi
+
 get_py_version(){
-curl https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/ 2>/dev/null | awk 'match($0,"Miniconda" "-" "[0-9.]+" "-" "[A-Za-z]+" "-" "[A-Za-z0-9_]+") { a = substr($0,RSTART,RLENGTH)
+cat tmp/a | awk 'match($0,"Miniconda" "-" "[0-9.]+" "-" "[A-Za-z]+" "-" "[A-Za-z0-9_]+") { a = substr($0,RSTART,RLENGTH)
   split(a,b,"-")
   print b[2] "-" b[1] ":\n  " b[3] "-" b[4] ":\n    sha:"
 }'
 
-curl -H "Accept-Encoding: gzip" https://downloads.python.org/pypy/ 2>/dev/null | gunzip | more | awk ' match($0, "pypy[0-9.]+" "-" "v[0-9.]+" "-" "[a-z0-9]+" ){
+cat tmp/b | awk ' match($0, "pypy[0-9.]+" "-" "v[0-9.]+" "-" "[a-z0-9]+" ){
   a = substr($0,RSTART,RLENGTH)
   split(a,b,"-")
   if(b[3] != "src"){print b[2] "-" b[1] ":\n  " b[3] ":\n    sha:" }
